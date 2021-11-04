6e60eea3595c194db13a32ca45e4b3cee6471437
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-04 11:40:37 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dmitry Sharabin on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
