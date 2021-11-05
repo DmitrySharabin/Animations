a86dca70ed890b479d8945e75de7d00d10158ee8
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-05 20:47:32 +0300

    Day 33 — Animating gestures

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,22 +7,51 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    @State private var dragAmount = CGSize.zero
+//
+//    var body: some View {
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+////            .animation(.spring(), value: dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        withAnimation {
+//                            dragAmount = .zero
+//                        }
+//                    }
+//            )
+//    }
+//}
+
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
     @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-//        .frame(width: 200, height: 200)
-//        .animation(.default, value: enabled)
-        .animation(nil, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
-//        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
